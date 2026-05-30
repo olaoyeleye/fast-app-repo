3765f1a850b57418830ac769c0429a50d46de3a4
Olu <olaoyeleye@yahoo.co.uk>
2026-05-30 03:26:37 +0100

    updated ec2-nginx to have kubectl

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -2,10 +2,11 @@ resource "aws_instance" "nginx" {
   ami                    = var.ami
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public-kunle-subnet.id
-  vpc_security_group_ids = [aws_security_group.public-kunle-sg.id]
+  vpc_security_group_ids = [aws_security_group.public-kunle-sg.id] 
 
   # Attach the IAM role
   iam_instance_profile   = aws_iam_instance_profile.ec2_eks_profile.name
+  
 
   user_data = <<-EOF
     #!/bin/bash
